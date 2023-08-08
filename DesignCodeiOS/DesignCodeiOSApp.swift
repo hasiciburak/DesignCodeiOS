37560a595e1935feb3497336f62cb4e8b334dcb7
Burak Haşıcı <burakhasici@gmail.com>
2023-08-08 23:20:46 +0300

    Initial Commit

---
+++ DesignCodeiOS/DesignCodeiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOSApp.swift
+//  DesignCodeiOS
+//
+//  Created by Burak Haşıcı on 8.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
