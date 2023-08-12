7718c29137f4b1859852b103e081c02afb99680e
Burak Haşıcı <burakhasici@gmail.com>
2023-08-13 00:37:51 +0300

    Added gitignore file and design card arrangements made.

--- DesignCodeiOS/ContentView.swift
+++ DesignCodeiOS/ContentView.swift
@@ -15,26 +15,33 @@ struct ContentView: View {
                 .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
+                .cornerRadius(20.0)
             Text("SwiftUI for iOS 16")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-            Text("20 sections - 3hours")
+            Text("20 sections - 3hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
+                .foregroundColor(.secondary)
             Text("Build an iOS app for iOS 16 with custom layouts and ...")
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
         }
-        .padding(.all,20.0)
+        .padding(.all, 20.0)
         .frame(height: 350.0)
         .background(Color("Background"))
         .cornerRadius(30.0)
-        .shadow(radius: /*@START_MENU_TOKEN@*/20/*@END_MENU_TOKEN@*/)
+        .shadow(color: Color("Shadow")
+            .opacity(0.3), radius: 10, x: 10, y: 10)
+        .padding(.horizontal, 20.0)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        ContentView()
         ContentView()
     }
 }
