429b4ea09cc8cdb88efdff842cc0c38f66b80d5c
Burak Haşıcı <burakhasici@gmail.com>
2023-08-12 18:50:31 +0300

    Added Project's First Files

--- DesignCodeiOS/ContentView.swift
+++ DesignCodeiOS/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+            Text("SwiftUI for iOS 16")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("20 sections - 3hours")
+                .font(.footnote)
+                .fontWeight(.semibold)
+            Text("Build an iOS app for iOS 16 with custom layouts and ...")
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
         }
-        .padding()
+        .padding(.all,20.0)
+        .frame(height: 350.0)
+        .background(Color("Background"))
+        .cornerRadius(30.0)
+        .shadow(radius: /*@START_MENU_TOKEN@*/20/*@END_MENU_TOKEN@*/)
     }
 }
 
