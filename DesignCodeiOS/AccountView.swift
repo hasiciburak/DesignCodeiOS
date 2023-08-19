acf3c35fcdd623091be64f10bbfcd18430b8eb0e
Burak <burakhasici@gmail.com>
2023-08-19 14:05:13 +0300

    Account view added

--- DesignCodeiOS/AccountView.swift
+++ DesignCodeiOS/AccountView.swift
@@ -9,8 +9,43 @@ import SwiftUI
 
 struct AccountView: View {
     var body: some View {
-        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                VStack {
+                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                        .symbolVariant(.circle.fill)
+                        .font(.system(size: 32))
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.blue, .blue.opacity(0.3))
+                        .padding()
+                        .background(Circle().fill(.ultraThinMaterial))
+                        .background(
+                            Image(systemName: "hexagon")
+                                .symbolVariant(.fill)
+                                .foregroundColor(.blue)
+                                .font(.system(size: 200))
+                                .offset(x: -50, y: -100)
+                        )
+                    Text("Burak Haşıcı")
+                        .font(.title.weight(.semibold))
+                    HStack {
+                        Image(systemName: "location")
+                            .imageScale(.small)
+                        Text("Turkey")
+                            .foregroundColor(.secondary)
+                    }
+                }.frame(maxWidth: .infinity)
+                
+                Section {
+                    Label("Settings", systemImage: "gear")
+                    Label("Billing", systemImage: "creditcard")
+                    Label("Help", systemImage: "questionmark")
+                }
+                .listRowSeparatorTint(.blue)
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Account")
         }
     }
 }
